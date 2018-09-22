54173ef0055f647de21a192c25789463903bdd03
Tanja Bendiek <tanjabendiek@MAC068.local>
2018-09-22 14:18:48 +0200

    first commit

---
+++ vampire_world.py
@@ -0,0 +1,6 @@
+print ('The world is beautiful! Enjoy the vampires!')
+regime = input('Do you prefer night or day?')
+name = input('What is your name?')
+
+print(f'Your name is {name} and you are awake at {regime}')
+
